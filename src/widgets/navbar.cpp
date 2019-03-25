862ab2b25ee1811377ad24a4e857c73d55f52507
ChrisRzech <christopherrzechula@gmail.com>
2019-03-25 00:33:42 -0700

    NavBar docs (#51)
    ### Key features
    * Documentation for `NavBar`
    
    ### Future Improvements
    * More in-depth documentation
    
    ### Notes
    none

--- src/widgets/navbar.cpp
+++ src/widgets/navbar.cpp
@@ -2,7 +2,16 @@
 #include "navitem.hpp"
 #include <QPropertyAnimation>
 
-/* Constructor */
+/**
+ * @brief Constructor
+ *
+ * Constructs a NavBar object. It will resize its parent widget to
+ * whatever initial size it needs to be and its current height.
+ *
+ * @param parent The parent widget the bar will be contained in
+ * @param minWidth Minimum width the bar will shrink to
+ * @param maxWidth Maximum width the bar will expand to
+ */
 NavBar::NavBar(QWidget* parent, int minWidth, int maxWidth)
     : QListWidget(parent), m_minWidth(minWidth), m_maxWidth(maxWidth)
 {
@@ -25,23 +34,54 @@ NavBar::NavBar(QWidget* parent, int minWidth, int maxWidth)
 
 }
 
-/* Setters */
+/**
+ * @brief Set height of the bar
+ *
+ * Sets the height of the bar and its parent.
+ * Not changing its parent will end up cutting off the bar.
+ *
+ * @param height The new height of the bar and its parent
+ */
 void NavBar::setHeight(int height)
 {
     resize(width(), height);
+    parentWidget()->resize(width(), height);
 }
 
+/**
+ * @brief Set minimum width of the bar
+ *
+ * Sets the minimum width of the bar.
+ *
+ * @param width The new minimum width
+ */
 void NavBar::setMinWidth(int width)
 {
     m_minWidth = width;
 }
 
+/**
+ * @brief Set maximum width of the bar
+ *
+ * Sets the maximum width of the bar.
+ *
+ * @param width The new maximum width
+ */
 void NavBar::setMaxWidth(int width)
 {
     m_maxWidth = width;
 }
 
-/* List modifiers */
+/**
+ * @brief Add button to the bar
+ *
+ * Adds a new button to the bar using the given strings.
+ * Since NavBar uses "Font Awesome 5 Free", all unicode
+ * string icons should be represented by that font.
+ *
+ * @param icon The unicode representation (ex. \uffff) of the icon using the"Font Awesome 5 Free" font
+ * @param label The label of the button
+ */
 void NavBar::addItem(QString icon, QString label)
 {
     const QSize itemSize(70, 80);
@@ -59,7 +99,14 @@ void NavBar::addItem(QString icon, QString label)
     connect(this, &NavBar::shrink, navItem, &NavItem::shrink);
 }
 
-/* Events */
+/**
+ * @brief Shrinks the bar
+ *
+ * Overrides QWidget::leaveEvent(QEvent*).
+ * When the cursor leaves the widget, the bar shrinks.
+ * The parent widget is also resizes to prevent cutting off.
+ * The NavBar::shrink() signal is emitted to tell its buttons to hide its labels.
+ */
 void NavBar::leaveEvent(QEvent*)
 {
     /* Shrinking animation for parent widget */
@@ -80,6 +127,14 @@ void NavBar::leaveEvent(QEvent*)
     emit shrink();
 }
 
+/**
+ * @brief Expands the bar
+ *
+ * Overrides QWidget::enterEvent(QEvent*).
+ * When the cursor enters the widget, the bar expands.
+ * The parent widget is also resizes to prevent overlaying issues.
+ * The NavBar::expand() signal is emitted to tell its buttons to show its labels.
+ */
 void NavBar::enterEvent(QEvent*)
 {
     /* Expanding animation for parent widget */
--- src/widgets/navbar.hpp
+++ src/widgets/navbar.hpp
@@ -1,6 +1,25 @@
 #pragma once
 #include <QListWidget>
 
+/**
+ * @class NavBar class
+ *
+ * Provides an easy way to list clickable buttons.
+ * Currently, the class uses "Font Awesome 5 Free" for its icons.
+ *
+ * To use:
+ * @code{.cpp}
+ * NavBar* navbar = new NavBar(widget, 50, 100);
+ * navbar->addItems("\uffff", "Button");
+ * @endcode
+ *
+ * This will instantiate the bar under the widget with a
+ * minimum width of 50 and maximum width of 100.
+ * Then it will add a button with the icon "\uffff"
+ * from the "Font Awesome 5 Free" font and a label "Button".
+ *
+ * @see <a href="https://fontawesome.com/icons?d=gallery&m=free">Font Awesome icon reference</a>
+ */
 class NavBar : public QListWidget
 {
     Q_OBJECT
