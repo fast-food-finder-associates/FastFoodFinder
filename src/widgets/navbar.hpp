9e5f5ecc3efd4351ebd226c61574386550f097b1
Christopher Rzechula <christopherrzechula@gmail.com>
2019-03-24 17:57:47 -0700

    NavBar docs

--- src/widgets/navbar.hpp
+++ src/widgets/navbar.hpp
@@ -1,6 +1,22 @@
 #pragma once
 #include <QListWidget>
 
+/**
+ * @brief NavBar class
+ *
+ * Provides an easy way to list clickable buttons.
+ * Currently, the class uses "Font Awesome 5 Free" for its icons.
+ *
+ * To use:
+ * @code{.cpp}
+ * NavBar* navbar = new NavBar(widget, 50, 100);
+ * navbar->addItems("\uf0ca", "Button");
+ * @endcode
+ *
+ * This will instantiate the bar under the widget with a
+ * minimum width of 50 and maximum width of 100.
+ * Then it will add a button called "Button".
+ */
 class NavBar : public QListWidget
 {
     Q_OBJECT
